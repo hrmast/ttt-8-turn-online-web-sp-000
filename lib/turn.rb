4f8cb6c9a26c0838899183d381921880bdf2fa29
Heath Mast <hrmast@gmail.com>
2020-03-18 04:08:42 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,48 @@
+#board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts = "Welcome to Tic Tac Toe"
+      puts " #{board[0]} | #{board[1]} | #{board[2]} "
+      puts "-----------"
+      puts " #{board[3]} | #{board[4]} | #{board[5]} "
+      puts "-----------"
+      puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+######################
+def valid_move?(board, index)
+  if index.between?(0,8)
+  if !position_taken?(board, index)
+    true
+end
+end
+end
+#####################
+def position_taken?(board, index)
+    if board[index] == "X" || board[index] == "O"
+      true
+    elsif board[index] == " " || ""
+      false
+    elsif board[index] == ""
+      false
+    else board[index] == nil
+      false
+    end
+  end
+##########################
+def move(array, index, value = "X" || "O")
+    array[index] = value
+end
+######################
+def input_to_index(input)
+  input.to_i - 1
+end
+##########################
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  input_to_index(input)
+  valid_move?(board, 0)
+
+  #move = 0
+  # while move < 8
+end
