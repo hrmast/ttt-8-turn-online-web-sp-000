7eb23062e3b692a6933bedbad1c0c37208e3fcf1
Heath Mast <hrmast@gmail.com>
2020-03-18 05:01:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,8 +41,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   input_to_index(input)
-  valid_move?(board,index)
-  break
+  valid_move?(board,0)
   #move = 0
   # while move < 8
 end
